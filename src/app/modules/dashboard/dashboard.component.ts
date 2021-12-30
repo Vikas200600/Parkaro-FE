d9b2156bfcb0cf5490f276a95fdfc7094c99067c
Vikas M <vikas.m@zysk.in>
2021-12-30 15:58:15 +0530

    dashboard component complete

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { LotService } from 'src/app/services/lot.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  today: Date = new Date();
+  time: string = new Date().toTimeString().split(' ')[0];
 
-  constructor() { }
+  occupied: number = 0;
+  available: number = 0;
+  availableSoon: number = 0;
+  overParked: number = 0;
 
-  ngOnInit(): void {
+  lotData: object;
+
+  constructor(private lotService: LotService) {
+    setInterval(() => {
+      this.time = new Date().toTimeString().split(' ')[0];
+    }, 1000);
+
+    // this.lotService.fetchUpdatedLots();
+    this.setLotData();
+    this.setStats();
   }
 
+  setLotData() {
+    this.lotService.lotSubject$.subscribe((lotData) => {
+      this.lotData = lotData;
+    });
+  }
+
+  setStats() {
+    this.lotData['keys'].forEach((key) => {
+      this.lotData[key].available
+        ? this.available++
+        : this.lotData[key].availableTime
+        ? this.lotData[key].availableTime - this.getNow() <= 900000 &&
+          this.lotData[key].availableTime - this.getNow() > 900000
+          ? this.availableSoon++
+          : this.overParked++
+        : this.occupied++;
+    });
+  }
+
+  getNow(): number {
+    return Date.now();
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('ngOnInit - this.lotData', this.lotData);
+    }, 0);
+  }
 }
--- src/app/services/lot.service.ts
+++ src/app/services/lot.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LotService {
+  lotSubject$ = new BehaviorSubject<object>({});
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.fetchUpdatedLots();
+  }
+
+  fetchUpdatedLots() {
+    this.http.get('/api/lots').subscribe((lotData: object) => {
+      this.lotSubject$.next(lotData['data']);
+    });
+  }
 }
